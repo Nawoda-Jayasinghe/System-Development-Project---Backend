c0b6f9558669d51c420801ab77445aa502c8762f
Nawoda Jayasinghe <nawoda.jayaasinghe98@gmail.com>
2023-04-18 05:27:31 +0530

    final

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,11 +1,32 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URI)
-    } catch (err) {
-        console.log(err)
-    }
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(process.env.DATABASE_URI)
+//     } catch (err) {
+//         console.log(err)
+//     }
+// }
+
+// module.exports = connectDB
+
+const mongoose = require('mongoose');
+
+class Database {
+  constructor() {
+    this._connect();
+  }
+
+  _connect() {
+    mongoose
+      .connect(process.env.DATABASE_URI)
+      .then(() => {
+        console.log('Connected to MongoDB');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
 
-module.exports = connectDB
+module.exports = new Database();
